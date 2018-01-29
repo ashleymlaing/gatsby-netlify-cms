4ffba0bd59d7c61b0c65c5b05f55fd52061870b8
Ashley Marie Laing <ashleym.laing@gmail.com>
2018-01-29 10:29:31 -0500

    added some content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,7 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => <div>
+  <h1>Ashley makes magic</h1>
+  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+  <img src="https://images.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.l-4xetVD1DssdQOlXrhXiQHaEK%26pid%3D15.1&f=1"/>
+</div>
